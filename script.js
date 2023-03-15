2a6e29773933beaae2326fb4dcf60c7a7ce78daa
Krishna Dass <43197751+KrishnaDass95@users.noreply.github.com>
2023-03-15 12:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ submitButtonElement.addEventListener('click', (e)=>{
 	e.preventDefault();
 	const age = document.getElementById('age').value;
 	const name = document.getElementById('name').value;
-	
+	let promise = new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			if(age > 18) {
+				resolve(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		}, 4000)
+	})
+	promise.then((res)=>{
+	alert(res);
+}).catch((e)=>{
+	alert(e);
+})
 })
